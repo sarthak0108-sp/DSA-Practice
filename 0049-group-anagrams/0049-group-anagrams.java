207e627e4c002a4e81b7e839cbf38e4cf4737df2
sarthak0108-sp <sarthaks0902@gmail.com>
2026-07-16 16:14:51 +0530

    Time: 7 ms (74.26%), Space: 49.7 MB (59.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,32 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-         int n=strs.length;
-         List<List<String>>ans=new ArrayList<>();
-         boolean vis[]=new boolean[n];
-         for(int i=0;i<n;i++){
-            if(vis[i])continue;
-            List<String>group=new ArrayList<>();
-            group.add(strs[i]);
-            vis[i]=true;
-            for(int j=i+1;j<n;j++){
-                if(!vis[j]&&isanagram(strs[i],strs[j])){
-                    group.add(strs[j]);
-                      vis[j]=true;
-                }
-              
+        HashMap<String,List<String>>map=new HashMap<>();
+        for(String s:strs){
+            char[]a=s.toCharArray();
+            Arrays.sort(a);
+            String key=new String(a);
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
             }
-            ans.add(group);
-         }
-         return ans;
-    }
-    boolean isanagram(String s1,String s2){
-        if(s1.length()!=s2.length()){
-            return false;
+            map.get(key).add(s);
         }
-        char []x=s1.toCharArray();
-        char []y=s2.toCharArray();
-        Arrays.sort(x);
-        Arrays.sort(y);
-        return  Arrays.equals(x,y);
+        return new ArrayList<>(map.values());
     }
 }
