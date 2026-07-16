573a098e60a28dd36aca2c83cc890cd453b4d38e
sarthak0108-sp <sarthaks0902@gmail.com>
2026-07-16 06:26:59 +0530

    Time: 5 ms (61.49%), Space: 44.3 MB (74.9%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int m=s.length();
+        int n=t.length();
+        if(m!=n)return false;
+        int freq[]=new int[26];
+        for(int i=0;i<m;i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+       return true;
+    }
+}
