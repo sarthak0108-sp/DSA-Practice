d21613625d168a58dcea88aa3181a033ab0b7897
sarthak0108-sp <sarthaks0902@gmail.com>
2026-07-20 09:55:59 +0530

    Time: 13 ms (34.67%), Space: 46.3 MB (31.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-        	return true;
-        }
-        int start = 0;
-        int last = s.length() - 1;
-        while(start <= last) {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) {
-        		start++;
-        	} else if(!Character.isLetterOrDigit(currLast)) {
-        		last--;
-        	} else {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
+        String rem=s.toLowerCase().replaceAll("[^a-z0-9]","");
+        int i=0;
+        int j=rem.length()-1;
+        while(i<j){
+            if(rem.charAt(i)!=rem.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
         }
         return true;
     }
